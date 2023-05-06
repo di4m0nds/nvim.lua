aa04474eb725bb66bf8b63fccaa7302583f506a6
di4m0nds <ws.2000.sja@gmail.com>
2023-05-06 10:14:20 -0300

    Stylized theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,8 +1,19 @@
+-- Initialization
 local chooseTheme = true
+local theme
 
+-- Colors
+local teal = "#209999"
+local gray = "#808080"
+local yellow = "#fcf403"
+
+local stringColor = { bg = "none", fg = teal }
+
+local lineNmbr = { bg = "none", fg = gray }
+local focusLineNmbr = { bg = "none", fg = yellow }
+
+-- Change Theme
 function SetTheme()
-  local theme
-
   if chooseTheme then
     theme = "gruber-darker"
     chooseTheme = false
@@ -13,9 +24,17 @@ function SetTheme()
 
   vim.cmd.colorscheme(theme)
 
+  -- [Customized Theme]
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  vim.api.nvim_set_hl(0, "String", { bg = "none", fg = "#626cff" })
+
+  -- Strings
+  vim.api.nvim_set_hl(0, "String", stringColor)
+
+  -- Line number
+  vim.api.nvim_set_hl(0, "LineNrAbove", lineNmbr)
+  vim.api.nvim_set_hl(0, "LineNr", focusLineNmbr)
+  vim.api.nvim_set_hl(0, "LineNrBelow", lineNmbr)
 end
 
 SetTheme()
