a4eb8663540d94005e8952f4fdc0d76347158c72
di4m0nds <ws.2000.sja@gmail.com>
2024-02-10 12:38:29 -0300

    Update undotree configuration

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,5 +1,9 @@
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
 
-vim.g.undotree_SplitWidth = 20
-vim.g.undotree_ShortIndicators = 1
-vim.g.undotree_RelativeTimestamp = 1
+vim.g.undotree_ShortIndicators    = 1
+vim.g.undotree_RelativeTimestamp  = 1
+vim.g.undotree_SplitWidth         = 10
+vim.g.undotree_WindowLayout       = 3
+vim.g.undotree_SetFocusWhenToggle = 1
+vim.g.undotree_DiffAutoOpen       = 0
+vim.g.undotree_DiffpanelHeight    = 5
